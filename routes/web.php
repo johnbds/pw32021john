99d58fb6602ca4f2ad20f83296e88f8cba5db8d6
johnbds <john.bds990@gmail.com>
2021-10-21 23:20:15 -0300

    aula 30/09

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/teste/{nome}', function ($nome){
 Route::get('/soma/{n1}/{n2}', function ($n1, $n2){
     return "<h1>A soma e: ".$n1+$n2."!</h1>";
 });
+// Fazendo a rota desta forma , deu erro :(
+//Route::get('genres', [\App\Http\Controllers\GenreController::class, 'index']);
+
+Route::resource('genres', \App\http\Controllers\GenreController::class);
